118ea03fd81943c60c330a42e3e6f4ac80afd913
Soe Wei Yan Phyo <soeweiyan.phyo>
2023-07-27 19:58:29 +1000

    Add btn ripple effect

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const btn = document.querySelector(".btn");
 
+let ripple;
+
+if (btn) {
+  btn.addEventListener("mouseenter", (e) => {
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+    // console.log(e.target.getBoundingClientRect());
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    btn.prepend(ripple);
+  });
+
+  btn.addEventListener("mouseleave", (e) => {
+    btn.removeChild(ripple);
+  });
+}
